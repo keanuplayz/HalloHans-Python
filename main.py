07fdb27596efe07d13f4f535fcb24852bc63e50d
Keanu <keanutimmermans8@gmail.com>
2020-01-24 14:18:07 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+"""CLI die hallo zegt
+Usage:
+    hello.py
+    hello.py <naam>
+    hello.py -h|--help
+    hello.py -v|--version
+Options:
+    <naam>  Optioneel naam argument.
+    -h --help  Dit scherm laten zien.
+    -v --version  Versie laten zien.
+"""
+
+from docopt import docopt
+
+def say_hello(name):
+    return("Dag {}!".format(name))
+
+def hans(name):
+    return("Hallo Hans!")
+
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='DEMO 1.0')
+    if arguments['<naam>']:
+        print(say_hello(arguments['<naam>']))
+    else:
+        print(arguments['--help'])
