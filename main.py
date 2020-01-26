0d4132c5f1ac5884539737eb8d22bb4e2d6ae1d4
keanuplayz <keanutimmermans8@gmail.com>
2020-01-26 19:16:37 +0100

    Fixed the CLI. It works now!

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-"""CLI die hallo zegt
+"""
+CLI die hallo zegt
+Versie 1.1.2
 Usage:
-    hello.py
-    hello.py <naam>
-    hello.py -h|--help
-    hello.py -v|--version
+    main.py
+    main.py -h  |--help
+    main.py -v | --version
 Options:
-    <naam>  Optioneel naam argument.
-    -h --help  Dit scherm laten zien.
+    -h --help  Dit help scherm laten zien.
     -v --version  Versie laten zien.
 """
 
 from docopt import docopt
 
-def say_hello(name):
-    return("Dag {}!".format(name))
-
 if __name__ == '__main__':
-    arguments = docopt(__doc__, version='DEMO 1.0')
-    if arguments['<naam>']:
-        print(say_hello(arguments['<naam>']))
+    arguments = docopt(__doc__, version='1.1.2')
 
-    if arguments['<naam>'] == 'Hans':
-        print("Hallo Hans!")
+name = input("Naam: ")
+if name == "Hans":
+    print("\n")
+    print("Hallo Hans!")
+else:
+    print("\n")
+    print("Dag " + name + "!")
